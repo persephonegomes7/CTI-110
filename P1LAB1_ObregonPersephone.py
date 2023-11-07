9ac62bdde31704eb3d667107e5c39c9ac256a086
persephonegomes7 <150168970+persephonegomes7@users.noreply.github.com>
2023-11-07 11:21:54 -0500

    First Upload

---
+++ P1LAB1_ObregonPersephone.py
@@ -0,0 +1,9 @@
+#Persephone Obregon
+#11/7/2023
+#Basic Input/Output
+
+#Get input from user
+name = input("Please enter your name: ")
+
+#Display output to the user
+print("Hello", name, "Welcome to CTI-110!")
